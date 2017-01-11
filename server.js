d04675793106c6f231024870b74e06c62d461943
Ruttathorn Panassorn <ruttathorn@hotmail.com>
2017-01-11 22:11:43 +0700

    Add POST for todo

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
+/*var todos = [{
 	id:1,
 	description: 'Meet mom for lunch',
 	completed: false
@@ -13,7 +15,11 @@ var todos = [{
 	id:3,
 	description: 'Go to home',
 	completed: true
-}];
+}]; */
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
 	res.send('Todo API root');
@@ -38,17 +44,18 @@ app.get('/todos/:id', function(req,res){
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
-	}
-	// Iterate of todos array. Fint the match.
-	
-	
-	//res.status(404).send();
-
-	res.send('Asking for todo with id of' + req.params.id)
+	}	
 });
+// POST /todos
+app.post('/todos',function(req,res){
+	var body = req.body;
+	body.id = todoNextId++;
 
-app.get('/todos')
+	todos.push(body);
+
+	res.json(body);
+});
 
 app.listen(PORT,function(){
 	console.log('Express listening on port' + PORT + '!');
-});
+}); 
